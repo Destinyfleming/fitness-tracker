d017fe58c549bf65e7a381f4d852586d60d96dab
Destiny Fleming <destiny.fleming7401@gmail.com>
2021-04-03 19:19:53 -0700

    added html router and start api routes

---
+++ routes/API.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const db = require('../models')
+
+router.get("/api/workouts", (req, res) => {
+    
+});
+
+router.put("/api/workouts/:workout", ({ params, body }, res) => {
+    
+});
+
+router.post('/api/workouts', (req,res) => {
+    
+});
+
+
+module.exports = router;
